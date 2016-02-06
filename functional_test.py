41cba9bf56b9c85129801e9925e628a950d2d98f
Pavel Pribyl <pribyl.pavel@gmail.com>
2016-02-06 17:39:35 +0100

    Simple URL routing, first unit test, FT update

--- functional_test.py
+++ functional_test.py
@@ -11,7 +11,18 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_todo_list_visible(self):
+		#Ramirez heard about our great site, so he enters the address to browser
+		self.browser.get('http://localhost:8000')
+		#He notices the To-Do in browser title and he likes that
 		self.assertIn('To-Do',self.browser.title)
+
+		#The site has this tempting imput box, so he tries entering
+		#his first to-do item "Buy soap"
+		
+
+		#The list nowcontains first item: "1. Buy soap" and Ramirez is happy
+		
+		self.fail('Stop the test for now ...')		
 
 if __name__ == '__main__':
 	unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+
+	def test_home_page_contains_todo_list(self):
+		request = HttpRequest()
+
+		response = home_page(request)
+		
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do Lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
+		
