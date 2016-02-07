743c009ec6f83dd0407307fb814aa858dc189806
Pavel Pribyl <pribyl.pavel@gmail.com>
2016-02-07 18:19:22 +0100

    Test models, repond with redirect

--- lists/tests.py
+++ lists/tests.py
@@ -13,15 +13,44 @@ class HomePageTest(TestCase):
 		#use decode to compare byte with string
 		self.assertEqual(response.content.decode(), expected_content)
 		
-	def test_home_page_process_post_requests(self):
+	def test_home_page_shows_items_from_db(self):
+		Item.objects.create(text='Item 1')
+		Item.objects.create(text='Item 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+		
+		self.assertIn('Item 1', response.content.decode())
+		self.assertIn('Item 2', response.content.decode())
+
+	def test_home_page_can_save_post_requests_to_db(self):
 		request = HttpRequest()
 		request.method = 'POST'
 		request.POST['item_text'] = 'New item'
 		
 		response = home_page(request)
 		
-		self.assertIn('New item',response.content.decode())
-		expected_content = render_to_string('home.html', {'new_item_text': 'New item'}, 
-			request=request)
-		self.assertEqual(response.content.decode(), expected_content)
+		item_from_db = Item.objects.all()[0]
+		self.assertEqual(item_from_db.text, 'New item')
+		
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['Location'], '/')
 
+from lists.models import Item
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_from_db(self):
+		first_item = Item()
+		first_item.text = 'Very first item'
+		first_item.save() 
+		
+		second_item = Item()
+		second_item.text = 'The second item'
+		second_item.save()
+
+		first_item_from_db = Item.objects.all()[0]
+		self.assertEqual(first_item_from_db.text, 'Very first item')
+
+		second_item_from_db = Item.objects.all()[1]
+		self.assertEqual(second_item_from_db.text, 'The second item')
+ 
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
+from lists.models import Item
 
 def home_page(request):
 	if request.method == 'POST':
 		new_item_text = request.POST['item_text']
-		return render(request, 'home.html', {'new_item_text': new_item_text}) 
+		Item.objects.create(text=new_item_text)
+		return redirect('/')
 	else:
-		return render(request, 'home.html', {'new_item_text': ''}) 
+		items = Item.objects.all()
+		return render(request, 'home.html', {'items': items}) 
